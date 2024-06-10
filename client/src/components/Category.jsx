d7ab30a8f22e74d3305df846483c0bfba9e3d9de
Robin <rirobink@gmail.com.com>
2024-06-10 22:39:08 +0600

    category img and name updates

--- client/src/components/Category.jsx
+++ client/src/components/Category.jsx
@@ -16,40 +16,41 @@ function Category(props) {
                     <div className="row">
 
                         <div className="col-md-3" >
-                            <CategoryItem color="success" name="Wallets" founds="57" imgurl="./Categoryimages/wallet.png" />
+                            <CategoryItem color="success" name="Personal Items" founds="57" imgurl="./Categoryimages/wallet.png" />
                         </div>
                         <div className="col-md-3  " >
-                            <CategoryItem color="danger" name="Wallets" founds="57" imgurl="./Categoryimages/wallet.png" />
+                            <CategoryItem color="danger" name="Electronics" founds="57" imgurl="./Categoryimages/smartphone.png" />
                         </div>
                         <div className="col-md-3" >
-                            <CategoryItem color="primary" name="Wallets" founds="57" imgurl="./Categoryimages/wallet.png" />
+                            <CategoryItem color="primary" name="Accessories" founds="57" imgurl="./Categoryimages/handbag.png" />
                         </div>
                         <div className="col-md-3  " >
-                            <CategoryItem color="info" name="Wallets" founds="57" imgurl="./Categoryimages/wallet.png" />
+                            <CategoryItem color="info" name="Clothings" founds="57" imgurl="./Categoryimages/jacket.png" />
                         </div>
                         <div className="col-md-3  " >
-                            <CategoryItem color="success" name="Wallets" founds="57" imgurl="./Categoryimages/wallet.png" />
+                            <CategoryItem color="success" name="Documents" founds="57" imgurl="./Categoryimages/visa.png" />
                         </div>
                         <div className="col-md-3  " >
-                            <CategoryItem color="danger" name="Wallets" founds="57" imgurl="./Categoryimages/wallet.png" />
+                            <CategoryItem color="danger" name="Vehicle" founds="57" imgurl="./Categoryimages/cycling.png" />
                         </div>
                         <div className="col-md-3  " >
-                            <CategoryItem color="primary" name="Wallets" founds="57" imgurl="./Categoryimages/wallet.png" />
+                            <CategoryItem color="primary" name="Pets" founds="57" imgurl="./Categoryimages/cat.png" />
                         </div>
                         <div className="col-md-3  " >
-                            <CategoryItem color="info" name="Wallets" founds="57" imgurl="./Categoryimages/wallet.png" />
+                            <CategoryItem color="info" name="Office Supplies" founds="57" imgurl="./Categoryimages/stationery.png" />
                         </div>
                         <div className="col-md-3  " >
-                            <CategoryItem color="success" name="Wallets" founds="57" imgurl="./Categoryimages/wallet.png" />
+                            <CategoryItem color="success" name="Medicine" founds="57" imgurl="./Categoryimages/medicine.png" />
                         </div>
                         <div className="col-md-3  " >
-                            <CategoryItem color="danger" name="Wallets" founds="57" imgurl="./Categoryimages/wallet.png" />
+                            <CategoryItem color="danger" name="Personal Care" founds="57" imgurl="./Categoryimages/make-up.png" />
+                        </div>
+                        
+                        <div className="col-md-3  " >
+                            <CategoryItem color="primary" name="Money" founds="57" imgurl="./Categoryimages/money.png" />
                         </div>
                         <div className="col-md-3  " >
-                            <CategoryItem color="primary" name="Wallets" founds="57" imgurl="./Categoryimages/wallet.png" />
-                        </div>
-                        <div className="col-md-3  " >
-                            <CategoryItem color="info" name="Wallets" founds="57" imgurl="./Categoryimages/wallet.png" />
+                            <CategoryItem color="info" name="Others" founds="57" imgurl="./Categoryimages/book.png" />
                         </div>
 
 
